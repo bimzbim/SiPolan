470f8668066ce7daf5fa88a7b3c673ada226c8dd
Purwa Adnyana <guspurwa123@gmail.com>
2023-12-03 21:46:03 +0800

    Creating CustomButton Composeable Function

---
+++ app/src/main/java/com/gatenzteam/sipolan/ui/component/CustomButton.kt
@@ -0,0 +1,48 @@
+package com.gatenzteam.sipolan.ui.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.gatenzteam.sipolan.R
+import com.gatenzteam.sipolan.ui.font.Poppins
+
+@Composable
+fun CustomIconButtom(
+    onClick: () -> Unit,
+    icon: ImageVector,
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(colorResource(id = R.color.color_palette3)),
+        shape = RoundedCornerShape(15.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = colorResource(R.color.color_palette1),
+            modifier = modifier
+                .padding(end = 10.dp)
+        )
+        Text(text = text,
+            fontSize = 14.83.sp,
+            fontFamily = Poppins.poppinsFamily,
+            color = colorResource(id = R.color.color_palette1)
+        )
+    }
+}
