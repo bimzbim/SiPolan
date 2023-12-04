1ecae19f1bb2a959e155ebecab55cb3cc49f73c3
Purwa Adnyana <guspurwa123@gmail.com>
2023-12-04 21:05:40 +0800

    Removing modifier testing in CustomIconButton and Fixing typo in CustomIconButtom composable

--- app/src/main/java/com/gatenzteam/sipolan/ui/activity/verification/VerificationActivity.kt
+++ app/src/main/java/com/gatenzteam/sipolan/ui/activity/verification/VerificationActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -170,9 +169,7 @@ class VerificationActivity : ComponentActivity() {
                     finish()
                 },
                 icon = Icons.Filled.LockReset,
-                text = "Forgot Password",
-                modifier = modifier
-                    .height(90.dp)
+                text = "Forgot Password"
             )
         }
     }
