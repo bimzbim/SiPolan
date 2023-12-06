32dc10470f69a5df4120883f7aeeec1ef0402cbb
Purwa Adnyana <guspurwa123@gmail.com>
2023-12-06 21:48:48 +0800

    Implement UI Design to Top App Bar

--- app/src/main/java/com/gatenzteam/sipolan/SiPolanApp.kt
+++ app/src/main/java/com/gatenzteam/sipolan/SiPolanApp.kt
@@ -1,19 +1,18 @@
 package com.gatenzteam.sipolan
 
+import android.os.Build
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -25,6 +24,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -34,6 +35,7 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.gatenzteam.sipolan.ui.navigation.NavigationItem
 import com.gatenzteam.sipolan.ui.navigation.Screen
@@ -56,9 +58,14 @@ fun SiPolanApp(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     Scaffold(
         topBar = {
-            TopAppBar(navController = navController)
+            TopAppBar(
+                navController = navController
+            )
         },
         bottomBar = {
             BottomBar(navController = navController)
@@ -106,30 +113,33 @@ fun SiPolanApp(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(navController: NavHostController) {
-    val profileImageModifier = Modifier
-        .size(40.dp)
-        .clip(CircleShape)
+fun TopAppBar(
+    navController: NavHostController
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
 
-    TopAppBar(
+    CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = colorpalette1,
             titleContentColor = colorpalette4,
         ),
         title = {
             Image(
-                painter = painterResource(id = R.drawable.logo_polan),
+                painter = painterResource(id = R.drawable.logo_polan_without_text),
                 contentDescription = null,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(40.dp)
+                    .height(64.dp)
+                    .padding(vertical = 10.dp)
             )
         },
         navigationIcon = {
-            IconButton(onClick = { navController.navigateUp() }) {
-                Icon(Icons.Default.KeyboardArrowLeft,
-                    contentDescription = "Back",
-                    tint = colorpalette3)
+            if(currentRoute != Screen.Home.route){
+                IconButton(onClick = { navController.navigateUp() }) {
+                    Icon(Icons.Default.KeyboardArrowLeft,
+                        contentDescription = "Back",
+                        tint = colorpalette3)
+                }
             }
         },
         actions = {
@@ -142,17 +152,14 @@ fun TopAppBar(navController: NavHostController) {
                     launchSingleTop = true
                 }
             }) {
-                Box(
+                Image(
+                    painter = painterResource(id = R.drawable.photo_profile),
+                    contentDescription = "Profile",
                     modifier = Modifier
-                        .background(MaterialTheme.colorScheme.primary)
-                        .then(profileImageModifier)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.photo_profile),
-                        contentDescription = "Profile",
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .align(Alignment.CenterVertically)
+                )
             }
         }
     )
@@ -165,8 +172,8 @@ fun BottomBar(
 ) {
     NavigationBar(
         modifier = modifier,
-        containerColor = colorpalette1
-
+        containerColor = colorpalette1,
+        contentColor = colorpalette3
     ){
         val navigationItem = listOf(
             NavigationItem(
