bfd699ba54cac244e0567c2dd99a839df1791b0c
Purwa Adnyana <guspurwa123@gmail.com>
2023-12-08 22:31:53 +0800

    HomeScreen Implement UI Design (Done), Preparing to use ArtikelScreen

--- app/src/main/java/com/gatenzteam/sipolan/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/gatenzteam/sipolan/ui/screen/home/HomeScreen.kt
@@ -3,12 +3,33 @@ package com.gatenzteam.sipolan.ui.screen.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.CarCrash
+import androidx.compose.material.icons.filled.Groups
+import androidx.compose.material.icons.filled.Traffic
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.gatenzteam.sipolan.ui.component.CustomText
 import com.gatenzteam.sipolan.ui.theme.ColorPalette1
+import com.gatenzteam.sipolan.ui.theme.ColorPalette2
+import com.gatenzteam.sipolan.ui.theme.ColorPalette3
 import com.gatenzteam.sipolan.ui.theme.ColorPalette4
 
 @Composable
@@ -17,11 +38,122 @@ fun HomeScreen(
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         modifier = modifier
             .fillMaxSize()
             .background(ColorPalette1)
+            .padding(horizontal = 25.dp)
     ){
-        Text(text = "Home", color = ColorPalette4)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier.padding(vertical = 20.dp)
+        ){
+            CustomText (
+                text = "Pelanggaran Hari Ini",
+                fontSize = 18.sp,
+                fontWeight = FontWeight(600),
+                color = ColorPalette4,
+                modifier = modifier
+                    .weight(1f)
+            )
+            IconButton(
+                onClick = { /*TODO*/ },
+                colors = IconButtonDefaults.iconButtonColors(
+                    containerColor = ColorPalette3,
+                    contentColor = ColorPalette1
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowForward,
+                    contentDescription = "Lihat deteksi pelanggaran hari ini"
+                )
+            }
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .fillMaxWidth()
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = modifier
+                    .background(ColorPalette2, RoundedCornerShape(15.dp))
+                    .weight(1f)
+                    .height(110.dp)
+            ){
+                Icon(
+                    imageVector = Icons.Filled.Traffic,
+                    contentDescription = "Pelanggaran Lampu Merah",
+                    tint = ColorPalette3,
+                    modifier = modifier
+                        .size(50.dp)
+                )
+                CustomText(text = "12")
+            }
+            Spacer(modifier = modifier.width(15.dp))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = modifier
+                    .background(ColorPalette2, RoundedCornerShape(15.dp))
+                    .weight(1f)
+                    .height(110.dp)
+                
+            ){
+                Icon(
+                    imageVector = Icons.Filled.CarCrash,
+                    contentDescription = "Pelanggaran Lampu Merah",
+                    tint = ColorPalette3,
+                    modifier = modifier
+                        .size(50.dp)
+                )
+                CustomText(text = "7")
+            }
+            Spacer(modifier = modifier.width(15.dp))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = modifier
+                    .background(ColorPalette2, RoundedCornerShape(15.dp))
+                    .weight(1f)
+                    .height(110.dp)
+            ){
+                Icon(
+                    imageVector = Icons.Filled.Groups,
+                    contentDescription = "Pelanggaran Lampu Merah",
+                    tint = ColorPalette3,
+                    modifier = modifier
+                        .size(50.dp)
+                )
+                CustomText(text = "24")
+            }
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier.padding(vertical = 20.dp)
+        ){
+            CustomText (
+                text = "Artikel Terbaru",
+                fontSize = 18.sp,
+                fontWeight = FontWeight(600),
+                color = ColorPalette4,
+                modifier = modifier
+                    .weight(1f)
+            )
+            IconButton(
+                onClick = { /*TODO*/ },
+                colors = IconButtonDefaults.iconButtonColors(
+                    containerColor = ColorPalette3,
+                    contentColor = ColorPalette1
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowForward,
+                    contentDescription = "Lihat deteksi pelanggaran hari ini"
+                )
+            }
+        }
     }
 }
+
