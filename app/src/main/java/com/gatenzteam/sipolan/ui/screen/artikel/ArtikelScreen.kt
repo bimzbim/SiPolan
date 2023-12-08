6413f1d34622ecf1c0452f2bb23a5032a2d71a1c
AdityaPrasetyoYogatama <yogathok51@gmail.com>
2023-12-08 21:13:39 +0700

    Finished ArtikelScreen

--- app/src/main/java/com/gatenzteam/sipolan/ui/screen/artikel/ArtikelScreen.kt
+++ app/src/main/java/com/gatenzteam/sipolan/ui/screen/artikel/ArtikelScreen.kt
@@ -5,16 +5,25 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -22,12 +31,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.gatenzteam.sipolan.R
+import com.gatenzteam.sipolan.ui.component.CustomText
 import com.gatenzteam.sipolan.ui.component.ScrollToTopButton
 import com.gatenzteam.sipolan.ui.navigation.Screen
-import com.gatenzteam.sipolan.ui.screen.riwayat_bayar.DataBayar
 import com.gatenzteam.sipolan.ui.theme.ColorPalette1
+import com.gatenzteam.sipolan.ui.theme.ColorPalette3
+import com.gatenzteam.sipolan.ui.theme.ColorPalette4
 import kotlinx.coroutines.launch
 
 @Composable
@@ -53,7 +72,7 @@ fun ArtikelColumn(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
 ) {
-    Box(modifier = modifier) {
+    Box(modifier = modifier.padding(25.dp)) {
         val scope = rememberCoroutineScope()
         val listState = rememberLazyListState()
         val showButton: Boolean by remember {
@@ -61,10 +80,14 @@ fun ArtikelColumn(
         }
         LazyColumn(
             state = listState,
-            contentPadding = PaddingValues(bottom = 20.dp)
         ) {
-            items(DataBayar.dummy, key = { it.id }) { bayar ->
-
+            items(DataArtikel.dummy, key = { it.id }) { artikel ->
+                ArtikelListItem(
+                    onClick = { onClick() },
+                    judul = artikel.judul,
+                    img = artikel.img,
+                    modifier = modifier
+                )
             }
         }
 
@@ -90,8 +113,64 @@ fun ArtikelColumn(
 @Composable
 fun ArtikelListItem(
     onClick: () -> Unit,
-    judul: String
-
+    judul: String,
+    img: Int,
+    modifier: Modifier
 ) {
+    Card(
+        shape = RoundedCornerShape(15.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(250.dp)
+            .padding(bottom = 20.dp)
+    ) {
+        Box{
+            Image(
+                painter = painterResource(id = img),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .fillMaxSize()
+            )
 
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(15.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                CustomText(
+                    text = judul,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    color = ColorPalette4,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row {
+                    CustomText(
+                        text = "Selengkapnya",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 14.83.sp,
+                        color = ColorPalette3,
+                        modifier = modifier
+                            .clickable(onClick = onClick)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowRight,
+                        contentDescription = null,
+                        tint = colorResource(R.color.color_palette3)
+                    )
+                }
+
+            }
+        }
+    }
 }
+
+@Preview(name = "PreviewList")
+@Composable
+fun ProfileScreenPreview() {
+    ArtikelListItem(onClick = {}, judul = "Helm itu gak penting! Yakin Dek?", img = R.drawable.foto_artikel, modifier = Modifier)
+}
+
