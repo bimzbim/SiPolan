9c131a098b969cf4dba1fce4ec011b5fd168885a
AdityaPrasetyoYogatama <yogathok51@gmail.com>
2023-12-17 16:50:35 +0700

    Creating ApiService and Response for SignUp

--- app/src/main/java/com/gatenzteam/sipolan/data/network/retrofit/ApiService.kt
+++ app/src/main/java/com/gatenzteam/sipolan/data/network/retrofit/ApiService.kt
@@ -1,6 +1,7 @@
 package com.gatenzteam.sipolan.data.network.retrofit
 
 import com.gatenzteam.sipolan.data.network.response.GetArtikelResponse
+import com.gatenzteam.sipolan.data.network.response.SignUpResponse
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
@@ -13,4 +14,15 @@ interface ApiService {
         @Query("page") page: Int,
         @Query("limit") limit: Int
     ): GetArtikelResponse
+
+    @POST("5acfd00a-bf84-48c3-9a5e-8038117298c2")
+    suspend fun signUp(
+        @Body("fullname") fullname: String,
+        @Body("email") email: String,
+        @Body("vehicle_number_plate") vehicleNumberPlate: String,
+        @Body("password") password: String
+    ): SignUpResponse
+
+
+
 }
