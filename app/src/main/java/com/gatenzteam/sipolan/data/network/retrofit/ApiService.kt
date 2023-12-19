af9edb8f02dbc58ee8a880e99fd237ee887735fa
Purwa Adnyana <guspurwa123@gmail.com>
2023-12-19 21:18:43 +0800

    Rename login suspend function to signIn

--- app/src/main/java/com/gatenzteam/sipolan/data/network/retrofit/ApiService.kt
+++ app/src/main/java/com/gatenzteam/sipolan/data/network/retrofit/ApiService.kt
@@ -33,7 +33,7 @@ interface ApiService {
 
     @FormUrlEncoded
     @POST("5acfd00a-bf84-48c3-9a5e-8038117298c2")
-    suspend fun signup(
+    suspend fun signUp(
         @Field("fullname") fullname: String,
         @Field("email") email: String,
         @Field("vehicle_number_plate") vehicleNumberPlate: String,
@@ -42,7 +42,7 @@ interface ApiService {
 
     @FormUrlEncoded
     @POST("0f015c92-4cfc-4d7e-9ada-d9288384d77e")
-    suspend fun login(
+    suspend fun signIn(
         @Field("email") email: String,
         @Field("password") password: String
     ): LoginResponse
